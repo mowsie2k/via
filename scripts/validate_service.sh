d9ae7379950b6a571b05d4c8958106b80a564d59
mowsie2k <mowsie2k@protonmail.com>
2024-07-01 01:43:02 -0400

    Update validation script

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Validate service
-curl -Lsv -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" http://localhost || exit 1
+curl -Lsv -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" http://localhost/auth/login || exit 1
