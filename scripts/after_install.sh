331576deaec63e412567a79360e349629d18e16e
mowsie2k <mowsie2k@protonmail.com>
2024-07-16 04:08:44 -0400

    Update scripts to allow email verification

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -51,12 +51,12 @@ touch .env.nginx
 
 cat <<EOT > .env
 WEB_DOMAIN=https://via.vicivisionamerica.com
-VALID_EMAIL_DOMAINS=vicivisionamerica.com
+VALID_EMAIL_DOMAINS=
 AUTH_TYPE=basic
 # Uncomment the following lines if you need email verification
-# REQUIRE_EMAIL_VERIFICATION=true
-# SMTP_USER=<GMAIL_ACCOUNT_EMAIL_YOU_WANT_TO_SEND_VERIFICATION_EMAILS_WITH>
-# SMTP_PASS=<GMAIL_ACCOUNT_PW_YOU_WANT_TO_SEND_VERIFICATION_EMAILS_WITH>
+REQUIRE_EMAIL_VERIFICATION=true
+SMTP_USER=forms@vicivisionamerica.com
+SMTP_PASS=QN5h96PCa*<a7bP%
 # Uncomment the following lines if using Google OAuth
 # AUTH_TYPE=google_oauth
 # GOOGLE_OAUTH_CLIENT_ID=809005354337-61c4mse4l4r75m9u6ukeh7rogm008sr0.apps.googleusercontent.com
