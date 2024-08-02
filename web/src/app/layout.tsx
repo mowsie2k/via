51c769941cca864ac96da9c4d77a04a916601aaa
mowsie2k <mowsie2k@protonmail.com>
2024-08-01 21:29:22 -0400

    Update branding

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata(): Promise<Metadata> {
     dynamicSettings.enterpriseSettings &&
     dynamicSettings.enterpriseSettings?.use_custom_logo
       ? "/api/enterprise-settings/logo"
-      : buildClientUrl("/danswer.ico");
+      : "/vv-logo.png";
 
   return {
     title: dynamicSettings.enterpriseSettings?.application_name ?? "VIA",
