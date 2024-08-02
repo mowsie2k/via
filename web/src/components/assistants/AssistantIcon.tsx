d0ba4e94797131ae1f2041ce733fa4c8815c7d59
mowsie2k <mowsie2k@protonmail.com>
2024-08-01 22:59:08 -0400

    Update branding

--- web/src/components/assistants/AssistantIcon.tsx
+++ web/src/components/assistants/AssistantIcon.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Tooltip } from "../tooltip/Tooltip";
 import { createSVG } from "@/lib/assistantIconUtils";
 import { buildImgUrl } from "@/app/chat/files/images/utils";
-import vvLogo from "../../../public/vv-logo.png"
 
 export function darkerGenerateColorFromId(id: string): string {
   const hash = Array.from(id).reduce(
@@ -18,13 +17,6 @@ export function darkerGenerateColorFromId(id: string): string {
 
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
-/*
-export function AssistantIcon({
-  assistant,
-  size,
-  border,
-}: {
-  assistant: Persona;
 
 export function AssistantIcon({
   assistant,
@@ -35,17 +27,41 @@ export function AssistantIcon({
   size?: "small" | "medium" | "large";
   border?: boolean;
 }) {
+  const color = darkerGenerateColorFromId(assistant.id.toString());
+
   return (
     <Tooltip delayDuration={1000} content={assistant.description}>
-      <img
-        className={`object-cover object-center rounded-sm overflow-hidden transition-opacity duration-300 opacity-100
-          ${size === "large" ? "w-8 h-8" : "w-6 h-6"}
-          ${border ? "ring ring-[1px] ring-border-strong" : ""}`}
-        src={vvLogo}
-        alt={assistant.name}
-        loading="lazy"
-      />
+      {
+        // Prioritization order: image, graph, defaults
+        assistant.uploaded_image_id ? (
+          <img
+            className={`object-cover object-center rounded-sm overflow-hidden transition-opacity duration-300 opacity-100
+              ${size === "large" ? "w-8 h-8" : "w-6 h-6"}`}
+            src={buildImgUrl(assistant.uploaded_image_id)}
+            loading="lazy"
+          />
+        ) : assistant.icon_shape && assistant.icon_color ? (
+          <div
+            className={`flex-none
+                  ${border && "ring ring-[1px] ring-border-strong "}
+                  ${size === "large" ? "w-8 h-8" : "w-6 h-6"} `}
+          >
+            {createSVG(
+              { encodedGrid: assistant.icon_shape, filledSquares: 0 },
+              assistant.icon_color,
+              size == "large" ? 48 : 36
+            )}
+          </div>
+        ) : (
+          <div
+            className={`flex-none rounded-sm overflow-hidden
+                  ${border && "border border-.5 border-border-strong "}
+                  ${size === "large" && "w-12 h-12"}
+                  ${(!size || size === "small") && "w-6 h-6"} `}
+            style={{ backgroundColor: color }}
+          />
+        )
+      }
     </Tooltip>
   );
 }
-*/
